058c2d82e6f1bce9b1a360bc695b7e02d1205cf3
Venancio Magalhaes <venanciomagalhaesd@gmail.com>
2024-02-18 15:15:55 -0300

    -

--- routes/authenticate/auth.php
+++ routes/authenticate/auth.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Authenticate\AuthController;
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::put('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgotPassword');
-Route::put('/reset-password/{token}', [AuthController::class, 'resetPassword'])->name('forgotPassword');
+Route::put('/reset-password/{token}', [AuthController::class, 'resetPassword'])->name('resetPassword');
 Route::put('/verify-email/{token}', [AuthController::class, 'verifyEmail'])->name('verifyEmail');
 Route::group(['middleware' => 'auth:sanctum'], function (){
     Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
